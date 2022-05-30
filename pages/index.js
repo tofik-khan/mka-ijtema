9afc2943556cbaa865e79215cfc127139f1b7fae
Tofik Khan <tofikahmad@live.com>
2022-05-30 01:11:44 -0700

    Fixing cross origin issues

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato&family=Noto+Serif:wght@700&display=swap"
           rel="stylesheet"

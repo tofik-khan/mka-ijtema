a42cc579469103600caca58f8f7c3d57a7c788f1
Tofik Khan <tofikahmad@live.com>
2022-06-30 16:18:40 -0700

    Added register message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/register",
+        destination:
+          "https://mkausa.wufoo.com/forms/2022-national-khuddam-atfal-ijtema-registration/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

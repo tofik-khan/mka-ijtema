c66fbc85d21fad282de1b66a9579ddcd3ab23761
Tofik Khan <tofikahmad@live.com>
2022-08-09 16:46:21 -0700

    I give up....

--- components/programAtfal.js
+++ components/programAtfal.js
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-export default function ProgramKhuddam() {
+export default function ProgramAtfal() {
   return (
     <section>
       <Container className="my-5">

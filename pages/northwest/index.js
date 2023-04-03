53416e097bda8f71c92dc0abe27de10c7409c130
Tofik Khan <tofik.khan@mkausa.org>
2023-04-02 22:52:05 -0700

    Add info for Regional Ijtema

--- pages/northwest/index.js
+++ pages/northwest/index.js
@@ -4,10 +4,11 @@ import Header from "../../components/Header.js";
 import Nav from "../../components/Nav.js";
 import Footer from "../../components/Footer.js";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 import styled from "styled-components";
 import Image from "next/image.js";
+import Link from "next/link.js";
 
 const HeroContainer = styled.div`
   background-image: url("https://images.unsplash.com/photo-1618022325802-7e5e732d97a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1348&q=80") !important;
@@ -30,10 +31,35 @@ const H1 = styled.h1`
   text-transform: uppercase;
 `;
 
+const H2 = styled.h2`
+  font-family: "Noto Serif";
+  font-size: 36px;
+  color: white;
+`;
+
+const H3 = styled.h3`
+  font-family: "Lato";
+  font-size: 16px;
+  font-weight: 700;
+  text-decoration: underline #f6bc56 2px;
+`;
+
 const Label = styled.span`
   text-transform: none;
   font-family: "Lato";
   font-size: 18px;
+`;
+
+const ItalicText = styled.p`
+  font-size: 28px;
+  font-style: italic;
+  font-family: serif;
+`;
+
+const InfoButtonWrapper = styled.div`
+  font-size: 28px;
+  font-style: italic;
+  font-family: serif;
 `;
 
 export default function Page() {
@@ -67,6 +93,36 @@ export default function Page() {
             </Row>
           </Container>
         </HeroContainer>
+        <div className="my-5">
+          <Container>
+            <Row className="justify-content-center text-center">
+              <Col md={4} className="my-3">
+                <H3>Location</H3>
+                <ItalicText>Zypher Point, Lake Tahoe</ItalicText>
+              </Col>
+              <Col md={4} className="my-3">
+                <H3>Dates</H3>
+                <ItalicText>May 26 - 28</ItalicText>
+              </Col>
+              <Col md={4} className="my-3">
+                <H3>Theme</H3>
+                <ItalicText>Holy Quran - Our Guiding Light</ItalicText>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="text-center py-5 text-bg-dark">
+          <H2>Be a part of our Regional Ijtema</H2>
+          <InfoButtonWrapper className="py-2">
+            <Link
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfjMV9z-JzkNRmm-9juiIvxbz5ivuGqe8UHoyaWCcLcSLM18A/viewform"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button variant="outline-warning btn-lg">Register Now!</Button>
+            </Link>
+          </InfoButtonWrapper>
+        </div>
       </main>
       <Footer />
     </>

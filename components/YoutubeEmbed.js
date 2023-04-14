db3f68f0ddece0da5673a35a778274ff8dc4801b
Tofik Khan <tofik.khan@mkausa.org>
2023-04-13 23:50:01 -0700

    Add promo and flyer

--- components/YoutubeEmbed.js
+++ components/YoutubeEmbed.js
@@ -2,11 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "../styles/YoutubeEmbed.module.css";
 
-const YoutubeEmbed = ({ embedId }) => (
+const YoutubeEmbed = ({ embedId, width, height }) => (
   <div className={styles.videoResponsive}>
     <iframe
-      width="853"
-      height="480"
+      width={width || "853"}
+      height={height || "480"}
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
--- pages/northwest/index.js
+++ pages/northwest/index.js
@@ -10,6 +10,8 @@ import styled from "styled-components";
 import Image from "next/image.js";
 import Link from "next/link.js";
 
+import YoutubeEmbed from "../../components/YoutubeEmbed.js";
+
 const HeroContainer = styled.div`
   background-image: url("https://images.unsplash.com/photo-1618022325802-7e5e732d97a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1348&q=80") !important;
   background-repeat: no-repeat;
@@ -29,12 +31,15 @@ const H1 = styled.h1`
   font-size: 72px;
 
   text-transform: uppercase;
+
+  @media screen and (max-width: 480px) {
+    font-size: 60px;
+  }
 `;
 
 const H2 = styled.h2`
   font-family: "Noto Serif";
   font-size: 36px;
-  color: white;
 `;
 
 const H3 = styled.h3`
@@ -126,13 +131,37 @@ export default function Page() {
         <div className="py-5">
           <Container>
             <Row className="align-items-center">
-              <Col>
+              <Col md={6}>
                 <h2>Join your brothers...</h2>
                 ...For a weekend of spiritually and physically refreshing
                 activities.
               </Col>
-              <Col>
+              <Col md={6}>
                 <Image src={"/nw-ijtema/flyer.jpeg"} width={400} height={500} />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="py-5" style={{ backgroundColor: "#EEEEEE" }}>
+          <Container>
+            <Row className="align-items-center">
+              <Col md={4}>
+                <Image
+                  src={"/nw-ijtema/ijtema-guest-invite.jpeg"}
+                  width={400}
+                  height={400}
+                />
+              </Col>
+              <Col md={4}>
+                <Image
+                  src={"/nw-ijtema/existence-project.jpeg"}
+                  width={400}
+                  height={400}
+                />
+              </Col>
+              <Col xs={{ order: "first" }} md={4} className="py-2">
+                <H2 className="text-center pb-3">Ijtema Promo!</H2>
+                <YoutubeEmbed embedId="GlEklHSmSNs" width={400} height={400} />
               </Col>
             </Row>
           </Container>

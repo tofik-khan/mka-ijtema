12b64e30eadec4134c627e1c8b1edf607fec2e44
Tofik Khan <tofik.khan@mkausa.org>
2023-04-29 07:09:10 -0700

    Add Header & Footer to Stats page

--- pages/stats.js
+++ pages/stats.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../partials/Header";
+import Footer from "../partials/Footer";
 
 export default function Page() {
   return (
     <>
+      <Header data={{ title: "National Ijtema 2023 | Stats" }} />
       <h1>Page</h1>
+      <Footer />
     </>
   );
 }

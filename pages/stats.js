14e0ddcab1c9a5456130a3254a065f2c9ecca3b6
Tofik Khan <tofik.khan@mkausa.org>
2023-04-29 07:14:30 -0700

    Add ServerSideProps to stats page

--- pages/stats.js
+++ pages/stats.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../partials/Header";
 import Footer from "../partials/Footer";
 
-export default function Page() {
+export default function Page({ data }) {
   return (
     <>
       <Header data={{ title: "National Ijtema 2023 | Stats" }} />
@@ -10,4 +10,12 @@ export default function Page() {
       <Footer />
     </>
   );
+}
+
+export async function getServerSideProps() {
+  let data = {};
+  await fetch("/api/typeform-pull")
+    .then((response) => response.json())
+    .then((response) => (data = response));
+  return { props: { data } };
 }

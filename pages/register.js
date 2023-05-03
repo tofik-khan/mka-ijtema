94be3fb840a735d792d421287b13a1c86628f073
Tofik Khan <tofik.khan@mkausa.org>
2023-05-02 22:19:00 -0700

    Update style import to not use legacy styles

--- pages/register.js
+++ pages/register.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header.js";
 import Nav from "../components/Nav.js";
 
-import * as styles from "../styles/StatsHero.module.css";
+import * as styles from "../styles/Hero.module.css";
 
 import Image from "next/image";
 

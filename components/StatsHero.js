9ce00c0cec05b4b2a005c0a2fac7f00147c3dab9
Tofik Khan <tofik.khan@mkausa.org>
2023-05-02 22:11:57 -0700

    Create Styled Components for Stats Hero

--- components/StatsHero.js
+++ components/StatsHero.js
@@ -6,27 +6,55 @@ import Col from "react-bootstrap/Col";
 
 import Image from "next/image";
 
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: black;
+  min-height: 40vh;
+
+  padding: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledHeading = styled.h1`
+  font-family: "Noto Serif";
+  font-size: 72px;
+  font-weight: 600;
+  text-transform: uppercase;
+`;
+
+const StyledHeadingLabel = styled.span`
+  text-transform: none;
+  font-family: "Lato";
+  font-size: 18px;
+`;
+
 export default function StatsHero() {
   return (
-    <div className={styles.container}>
+    <StyledContainer>
       <Container>
         <Row className="text-center text-md-left">
           <Col sm={12} md={6}>
             <Image src="/Ijtema-logo.png" width={300} height={300} />
           </Col>
           <Col sm={12} md={6}>
-            <h1 className={styles.heading}>
-              <span className={styles.headingLabel}>
+            <StyledHeading>
+              <StyledHeadingLabel>
                 Majlis Khuddam-ul-Ahmadiyya USA
-              </span>
+              </StyledHeadingLabel>
               <br />
               Ijtema
               <br />
               Stats
-            </h1>
+            </StyledHeading>
           </Col>
         </Row>
       </Container>
-    </div>
+    </StyledContainer>
   );
 }

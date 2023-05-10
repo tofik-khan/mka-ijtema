ea3684498b7cce037d4e8ee27363f82f99dba991
Tofik Khan <tofik.khan@mkausa.org>
2023-05-10 12:27:27 -0700

    Add Accommodations info to index

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import RegistrationCTA from "../components/RegistrationCTA.js";
+import { Button } from "react-bootstrap";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -44,6 +46,41 @@ export default function Home() {
           <Row className="justify-content-center">
             <Col xs={10} md={8}>
               <YoutubeEmbed embedId="EjOzV1tELbc" />
+            </Col>
+          </Row>
+        </Container>
+        <Container className="mt-3 py-5 bg-light" fluid>
+          <Row className="justify-content-center">
+            <Col md={8}>
+              <h2 className="text-center">Accommodations</h2>
+              <p>
+                While on-site sleeping arrangements are readily available at
+                Masjid Bait-ur-Rahman for Ijtema, some may prefer the
+                convenience of hotel accommodations. We have negotiated a rate
+                for our Ijtema with Residence Inn (5 miles away). See details
+                below:{" "}
+              </p>
+              <p>
+                Residence Inn by Marriott (12000 Plum Orchard Drive, Silver
+                Spring, MD 20904): $159 + tax per night using the below link or
+                by calling: 301-572-2322. Guests should mention the discount
+                code: MKA USA ROOM BLOCK. This code is valid from Friday, June
+                23 (Check in) - Sunday, June 25 (Check out).{" "}
+              </p>
+              <p>
+                Rooms can sleep 3-4 guests. Attendees are encouraged to book
+                with their friends and relatives to save costs. The deadline to
+                lock in the rates is June 1, 2023. Rooms are limited. If you
+                have any questions, please reach out to Ali Chaudhry
+                (240-997-6262).
+              </p>
+              <Link
+                href="https://www.marriott.com/events/start.mi?id=1681742740326&key=GRP"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button variant="dark">Reserve Hotel Room</Button>
+              </Link>
             </Col>
           </Row>
         </Container>
